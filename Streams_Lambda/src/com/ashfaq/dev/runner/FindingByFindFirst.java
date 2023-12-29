a189c135df0745c717fd5ce52cf0b2bc38e45d67
Ashfaqbs <ash001x@gmail.com>
2023-12-29 19:24:54 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/runner/FindingByFindFirst.java
+++ Streams_Lambda/src/com/ashfaq/dev/runner/FindingByFindFirst.java
@@ -7,6 +7,20 @@
 
 package com.ashfaq.dev.runner;
 
+import java.util.Optional;
+
+import com.ashfaq.dev.models.User;
+import com.ashfaq.dev.utils.ListData;
+
 public class FindingByFindFirst {
 
+	public static void main(String[] args) {
+		
+		Optional<User> userWithId3 =ListData.listLoader().stream()
+		        .filter(user -> user.getId() == 3)
+		        .findFirst();
+
+		System.out.println(userWithId3);
+		
+	}
 }
