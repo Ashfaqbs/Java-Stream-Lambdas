f74bbd7ccc15790ddfa4c7c88acca88ed3fa33d3
Ashfaqbs <ash001x@gmail.com>
2023-12-29 19:02:51 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/utils/ListData.java
+++ Streams_Lambda/src/com/ashfaq/dev/utils/ListData.java
@@ -24,6 +24,61 @@ public class ListData {
 		sampleUserList.add(new User(6, "Jony", "Samal", "SchoolA"));
 		sampleUserList.add(new User(7, "Sony", "Samal", "SchoolB"));
 		sampleUserList.add(new User(8, "Katy", "Samal", "SchoolB"));
+		
+		sampleUserList.add(new User(1, "John", "Doe", "High School A"));
+		sampleUserList.add(new User(2, "Jane", "Smith", "High School B"));
+		sampleUserList.add(new User(3, "Bob", "Johnson", "High School A"));
+		sampleUserList.add(new User(4, "Alice", "Williams", "High School C"));
+		sampleUserList.add(new User(5, "Charlie", "Brown", "High School B"));
+		sampleUserList.add(new User(6, "Eva", "Green", "High School A"));
+		sampleUserList.add(new User(7, "Frank", "Miller", "High School B"));
+		sampleUserList.add(new User(8, "Grace", "Turner", "High School B"));
+
+		// Adding 42 more real-looking users
+		sampleUserList.add(new User(9, "David", "Jones", "High School C"));
+		sampleUserList.add(new User(10, "Emma", "Johnson", "High School A"));
+		sampleUserList.add(new User(11, "William", "Lee", "High School B"));
+		sampleUserList.add(new User(12, "Sophia", "Martin", "High School A"));
+		sampleUserList.add(new User(13, "James", "Garcia", "High School C"));
+		sampleUserList.add(new User(14, "Olivia", "Davis", "High School B"));
+		sampleUserList.add(new User(15, "Liam", "Moore", "High School A"));
+		sampleUserList.add(new User(16, "Ava", "Rodriguez", "High School B"));
+		sampleUserList.add(new User(17, "Henry", "Lopez", "High School C"));
+		sampleUserList.add(new User(18, "Chloe", "Hill", "High School A"));
+		sampleUserList.add(new User(19, "Daniel", "Ward", "High School B"));
+		sampleUserList.add(new User(20, "Mia", "Baker", "High School A"));
+		sampleUserList.add(new User(21, "Logan", "Sanders", "High School C"));
+		sampleUserList.add(new User(22, "Emily", "Carter", "High School B"));
+		sampleUserList.add(new User(23, "Jackson", "Barnes", "High School A"));
+		sampleUserList.add(new User(24, "Grace", "Hayes", "High School B"));
+		sampleUserList.add(new User(25, "Lucas", "Hunt", "High School A"));
+		sampleUserList.add(new User(26, "Lily", "West", "High School C"));
+		sampleUserList.add(new User(27, "Benjamin", "Harrison", "High School B"));
+		sampleUserList.add(new User(28, "Avery", "Fisher", "High School A"));
+		sampleUserList.add(new User(29, "Caleb", "Evans", "High School B"));
+		sampleUserList.add(new User(30, "Scarlett", "Dixon", "High School A"));
+		sampleUserList.add(new User(31, "Mason", "Sullivan", "High School C"));
+		sampleUserList.add(new User(32, "Ella", "Pearson", "High School B"));
+		sampleUserList.add(new User(33, "Nathan", "Floyd", "High School A"));
+		sampleUserList.add(new User(34, "Hannah", "Owens", "High School B"));
+		sampleUserList.add(new User(35, "Wyatt", "Reed", "High School C"));
+		sampleUserList.add(new User(36, "Addison", "Ferguson", "High School A"));
+		sampleUserList.add(new User(37, "Andrew", "Pierce", "High School B"));
+		sampleUserList.add(new User(38, "Aria", "Hudson", "High School A"));
+		sampleUserList.add(new User(39, "Christopher", "Gibson", "High School C"));
+		sampleUserList.add(new User(40, "Aubrey", "Perkins", "High School B"));
+		sampleUserList.add(new User(41, "Nicholas", "Wheeler", "High School A"));
+		sampleUserList.add(new User(42, "Paisley", "Murray", "High School B"));
+		sampleUserList.add(new User(43, "Owen", "Walters", "High School A"));
+		sampleUserList.add(new User(44, "Brooklyn", "Baldwin", "High School C"));
+		sampleUserList.add(new User(45, "Gabriel", "Fleming", "High School B"));
+		sampleUserList.add(new User(46, "Savannah", "Hodge", "High School A"));
+		sampleUserList.add(new User(47, "Isaac", "Thornton", "High School B"));
+		sampleUserList.add(new User(48, "Madison", "Walsh", "High School A"));
+		sampleUserList.add(new User(49, "Dylan", "Perry", "High School C"));
+		sampleUserList.add(new User(50, "Zoe", "Harvey", "High School B"));
+
+		
 
 		return sampleUserList;
 	}
