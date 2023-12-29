6f540d90b099f8be5df4fef3ec23ad70672174a1
Ashfaqbs <ash001x@gmail.com>
2023-12-29 18:53:16 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/utils/UserMapper.java
+++ Streams_Lambda/src/com/ashfaq/dev/utils/UserMapper.java
@@ -7,6 +7,14 @@
 
 package com.ashfaq.dev.utils;
 
+import com.ashfaq.dev.models.User;
+import com.ashfaq.dev.models.UserDTO;
+
 public class UserMapper {
 
+	
+	public static User maptoUser(UserDTO dto)
+	{
+		return new User(dto.getId(), dto.getfName(),dto.getlName(), dto.getSchoolName());
+	}
 }
