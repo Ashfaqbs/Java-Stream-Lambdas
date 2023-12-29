aa892055611ce00da530063ce3b8538a4cdda006
Ashfaqbs <ash001x@gmail.com>
2023-12-29 18:56:01 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/runner/StreamAndMethodRef_UC1.java
+++ Streams_Lambda/src/com/ashfaq/dev/runner/StreamAndMethodRef_UC1.java
@@ -3,18 +3,21 @@
  * GitHub: https://github.com/DarkSharkAsh
  */
 
-
-
 package com.ashfaq.dev.runner;
+
+import java.util.List;
+
+import com.ashfaq.dev.models.User;
+import com.ashfaq.dev.utils.ListData;
 
 public class StreamAndMethodRef_UC1 {
 
-	
 	public static void main(String[] args) {
-		
-		
-		
-		
+
+		List<User> listLoader = ListData.listLoader();
+
+//		DTO designPattern 
+
 	}
-	
+
 }
