5b8f2d1185579da0acc61ac0827b57b4c03ca595
Ashfaqbs <ash001x@gmail.com>
2023-12-29 19:07:05 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/runner/sortedByName.java
+++ Streams_Lambda/src/com/ashfaq/dev/runner/sortedByName.java
@@ -3,10 +3,21 @@
  * GitHub: https://github.com/DarkSharkAsh
  */
 
-
-
 package com.ashfaq.dev.runner;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.ashfaq.dev.models.User;
+import com.ashfaq.dev.utils.ListData;
 
 public class sortedByName {
 
+	public static void main(String[] args) {
+
+		List<User> sortedByName = ListData.listLoader().stream().sorted(Comparator.comparing(User::getfName))
+				.collect(Collectors.toList());
+
+	}
 }
