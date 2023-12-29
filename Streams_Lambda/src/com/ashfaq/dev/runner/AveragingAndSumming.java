8b5b4bb7db65bf519a8087bc0adb0b96f4f76aa0
Ashfaqbs <ash001x@gmail.com>
2023-12-29 19:28:26 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/runner/AveragingAndSumming.java
+++ Streams_Lambda/src/com/ashfaq/dev/runner/AveragingAndSumming.java
@@ -7,6 +7,25 @@
 
 package com.ashfaq.dev.runner;
 
+import com.ashfaq.dev.models.User;
+import com.ashfaq.dev.utils.ListData;
+
 public class AveragingAndSumming {
 
+	
+	public static void main(String[] args) {
+		
+		
+		double averageFirstNameLength = ListData.listLoader().stream()
+		        .mapToInt(user -> user.getfName().length())
+		        .average()
+		        .orElse(0.0);
+
+		
+		int sumOfUserIds = sampleUserList.stream()
+		        .mapToInt(User::getId)
+		        .sum();
+
+		
+	}
 }
