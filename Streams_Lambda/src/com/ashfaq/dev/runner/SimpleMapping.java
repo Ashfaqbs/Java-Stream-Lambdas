ad5a57d402c8c0b2d43228e69c2b423273d533d6
Ashfaqbs <ash001x@gmail.com>
2023-12-29 19:09:12 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/runner/SimpleMapping.java
+++ Streams_Lambda/src/com/ashfaq/dev/runner/SimpleMapping.java
@@ -7,6 +7,21 @@
 
 package com.ashfaq.dev.runner;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.ashfaq.dev.models.User;
+import com.ashfaq.dev.utils.ListData;
+
 public class SimpleMapping {
 
+	public static void main(String[] args) {
+		
+		List<String> userNames =ListData.listLoader().stream()
+		        .map(User::getfName)
+		        .collect(Collectors.toList());
+
+		
+	}
+	
 }
