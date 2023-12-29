f9da965879d4983a05e3ade70166b7598dccaf3b
Ashfaqbs <ash001x@gmail.com>
2023-12-29 19:04:55 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/runner/Filtering.java
+++ Streams_Lambda/src/com/ashfaq/dev/runner/Filtering.java
@@ -7,6 +7,18 @@
 
 package com.ashfaq.dev.runner;
 
-public class Filtering {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.ashfaq.dev.models.User;
+
+public class Filtering {
+ public static void main(String[] args) {
+	
+	 List<User> highSchoolAUsers = sampleUserList.stream()
+		        .filter(user -> "High School A".equals(user.getSchool()))
+		        .collect(Collectors.toList());
+
+	 
+}
 }
