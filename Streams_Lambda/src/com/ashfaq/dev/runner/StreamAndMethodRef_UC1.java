9b2cd1ab11c5491f39596b88aeca0fbb644350bb
Ashfaqbs <ash001x@gmail.com>
2023-12-29 18:57:47 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/runner/StreamAndMethodRef_UC1.java
+++ Streams_Lambda/src/com/ashfaq/dev/runner/StreamAndMethodRef_UC1.java
@@ -6,8 +6,10 @@
 package com.ashfaq.dev.runner;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.ashfaq.dev.models.User;
+import com.ashfaq.dev.models.UserDTO;
 import com.ashfaq.dev.utils.ListData;
 import com.ashfaq.dev.utils.UserMapper;
 
@@ -19,7 +21,7 @@ public class StreamAndMethodRef_UC1 {
 
 //		DTO designPattern USER -> USERDTO UserMapper::maptoUserDTO
 
-
+List<UserDTO> collectUser = listLoader.stream().map((data)-> UserMapper.maptoUserDTO(data)).collect(Collectors.toList());
 		
 		
 	
