c0caa132d7520bb26fef386253d0fcde40f94bb4
Ashfaqbs <ash001x@gmail.com>
2023-12-29 19:21:17 +0530

    General Commits

--- Streams_Lambda/src/com/ashfaq/dev/runner/Counting.java
+++ Streams_Lambda/src/com/ashfaq/dev/runner/Counting.java
@@ -3,8 +3,6 @@
  * GitHub: https://github.com/DarkSharkAsh
  */
 
-
-
 package com.ashfaq.dev.runner;
 
 import java.util.Map;
@@ -14,16 +12,14 @@ import com.ashfaq.dev.models.User;
 import com.ashfaq.dev.utils.ListData;
 
 public class Counting {
-	
-	public static void main(String[] args) {
-		
-		
-		
-		Map<String, Long> schoolUserCount = ListData.listLoader().stream()
-		        .collect(Collectors.groupingBy(User::getSchoolName, Collectors.counting()));
 
-System.out.println(schoolUserCount);		
-		
+	public static void main(String[] args) {
+
+		Map<String, Long> schoolUserCount = ListData.listLoader().stream()
+				.collect(Collectors.groupingBy(User::getSchoolName, Collectors.counting()));
+
+		System.out.println(schoolUserCount);
+
 	}
 
 }
