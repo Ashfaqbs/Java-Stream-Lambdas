af2d631b3c4e566ba1c2ea090734c6db141d4d01
Ashfaqbs <ash001x@gmail.com>
2024-08-15 20:46:03 +0530

    Prac collect(Collectors.partitioningBy())

---
+++ Streams_Lambda/src/com/ashfaq/dev/prac/P1.java
@@ -0,0 +1,31 @@
+package com.ashfaq.dev.prac;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class P1 {
+
+	public static void main(String[] args) {
+
+		List<Integer> intList = List.of(1, 2, 3, 4, 5);
+		int sum = intList.stream().mapToInt(Integer::intValue).sum();
+//		System.out.println(sum);
+		Integer reduce = intList.stream().reduce(0, (a1, a2) -> a1 + a2);
+//		System.out.println(reduce);
+
+//		IntStream.range(0, 10).forEach(System.out::println);
+
+		List<String> sampleStringnames = List.of("Sonu", "Tony", "Chintu");
+
+		String collect = sampleStringnames.stream().collect(Collectors.joining("-", ",", "/"));
+		System.out.println(collect);// ,Sonu-Tony-Chintu/
+
+		Map<Boolean, List<String>> collect2 = sampleStringnames.stream()
+				.collect(Collectors.partitioningBy(s -> s.length() > 4));
+
+		System.out.println(collect2);//{false=[Sonu, Tony], true=[Chintu]}
+
+	}
+
+}
