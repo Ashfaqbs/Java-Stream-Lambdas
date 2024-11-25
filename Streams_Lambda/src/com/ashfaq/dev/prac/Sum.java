7d26a9a815f1bfb4841e6878b1fbafded6af79ef
Ashfaqbs <ash001x@gmail.com>
2024-11-25 19:11:25 +0530

    - Creating a Custom Function Interface and running with Lambda and   stream   - Add example is used

---
+++ Streams_Lambda/src/com/ashfaq/dev/prac/Sum.java
@@ -0,0 +1,19 @@
+package com.ashfaq.dev.prac;
+
+public class Sum {
+	public static void main(String[] args) {
+
+		Calc x = (a, b) -> {
+
+			return a + b;
+		};
+
+		int sum = x.add(1, 2);
+		System.out.println(sum);
+	}
+}
+
+interface Calc {
+
+	int add(int a, int b);
+}
